ffd33ead040ae5a25a4c8a73cc2755282901933c
Ibrahim Alhamad <ibrahim@monease.io>
2026-01-18 18:54:52 +0100

    fix: format composites index

--- src/components/composites/index.ts
+++ src/components/composites/index.ts
@@ -1,13 +1,13 @@
 export { CollapseString } from "./collapse-string";
 export { ConfirmationDialog } from "./confirmation-dialog";
-export {
-	TintedBackgroundIcon,
-	type TintedBackgroundIconProps,
-	type TintedBackgroundIconSize,
-} from "./tinted-background-icon";
 export { CopyButton } from "./copy-button";
 export { DeleteConfirmationDialog } from "./delete-confirmation-dialog";
 export { DetailRow } from "./detail-row";
 export { ErrorBoundary } from "./error-boundary";
 export { Loader } from "./loader";
 export { StatusBadge } from "./status-badge";
+export {
+	TintedBackgroundIcon,
+	type TintedBackgroundIconProps,
+	type TintedBackgroundIconSize,
+} from "./tinted-background-icon";
